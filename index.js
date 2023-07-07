fdbb855bb79576015a145b68493fcf7281a42a4b
setkyar <setkyar16@gmail.com>
2023-07-07 22:16:46 +0800

    use .env for port

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 
+require("dotenv").config();
 const db = require("./config/db");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
